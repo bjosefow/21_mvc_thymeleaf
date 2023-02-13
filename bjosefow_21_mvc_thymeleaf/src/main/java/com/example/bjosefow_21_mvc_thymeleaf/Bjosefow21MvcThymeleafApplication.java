952f4227aa02a379af733d37078d3ad5bef16c0b
Betina Josefowicz <betina.josefowicz@gmail.com>
2023-02-13 16:15:23 +0100

    dodanie projektu

---
+++ bjosefow_21_mvc_thymeleaf/src/main/java/com/example/bjosefow_21_mvc_thymeleaf/Bjosefow21MvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.bjosefow_21_mvc_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bjosefow21MvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bjosefow21MvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ bjosefow_21_mvc_thymeleaf/src/test/java/com/example/bjosefow_21_mvc_thymeleaf/Bjosefow21MvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bjosefow_21_mvc_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bjosefow21MvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
